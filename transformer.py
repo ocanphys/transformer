7f8fc949e4d22cd5b3649ca1521e3c529ad21b51
oguz <oguzhan.can@outlook.com>
2026-06-01 23:00:07 -0700

    device fixes, uniform variable naming

--- transformer.py
+++ transformer.py
@@ -15,8 +15,8 @@ class Linear(nn.Module):
             nn.init.trunc_normal_(torch.empty((out_features, in_features), device=device, dtype=dtype), mean=0, std=std)
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:  # x: (B, d_in)
-        return einsum(self.W, x, "d_out d_in, batch ... d_in -> batch ... d_out")
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return einsum(self.W, x, "d_out d_in, ... d_in -> ... d_out")
 
 
 class Embedding(nn.Module):
@@ -55,22 +55,29 @@ class RMSNorm(nn.Module):
             torch.float32
         )  # since we will square numbers - to avoid overflow we upcast in case dtype has smaller headroom
         rms_a = ((x**2).mean(dim=-1, keepdim=True) + self.eps) ** 0.5  # keepdim keeps the last dimension
-        return torch.einsum("...d,d->...d", (x / rms_a).to(in_dtype), self.gain.to(in_dtype))
+        return einsum((x / rms_a).to(in_dtype), self.gain.to(in_dtype), "... d_model, d_model -> ... d_model")
 
 
 class SwiGLU(nn.Module):
-    def __init__(self, d_ff, d_model):
+    def __init__(self, d_ff, d_model, device: torch.device | None = None, dtype: torch.dtype | None = None):
         super().__init__()
-        self.W1 = nn.Parameter(torch.ones((d_ff, d_model)))
-        self.W2 = nn.Parameter(torch.ones((d_model, d_ff)))
-        self.W3 = nn.Parameter(torch.ones((d_ff, d_model)))
+        kaiming = lambda out_features, in_features: nn.Parameter(
+            nn.init.trunc_normal_(
+                torch.empty((out_features, in_features), device=device, dtype=dtype),
+                mean=0,
+                std=1 / in_features**0.5,
+            )
+        )
+        self.W1 = kaiming(d_ff, d_model)
+        self.W2 = kaiming(d_model, d_ff)
+        self.W3 = kaiming(d_ff, d_model)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        SiLU = lambda x: x * torch.sigmoid(x)
-        hadamard = SiLU(torch.einsum("f m, ... m -> ... f", self.W1, x)) * torch.einsum(
-            "f m, ... m -> ... f", self.W3, x
+        silu = lambda x: x * torch.sigmoid(x)
+        hadamard = silu(einsum(self.W1, x, "d_ff d_model, ... d_model -> ... d_ff")) * einsum(
+            self.W3, x, "d_ff d_model, ... d_model -> ... d_ff"
         )
-        return torch.einsum("m f, ... f -> ... m", self.W2, hadamard)
+        return einsum(self.W2, hadamard, "d_model d_ff, ... d_ff -> ... d_model")
 
 
 def SPDA(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, mask: torch.Tensor | None = None):
@@ -86,47 +93,19 @@ def SPDA(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, mask: torch.Tensor |
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    d_K = K.shape[-1]
-    attention_weights = einsum(Q, K, "... queries d_k, ... keys d_k -> ... queries keys") / (d_K**0.5)
+    d_k = K.shape[-1]
+    attention_weights = einsum(Q, K, "... seq_q d_k, ... seq_k d_k -> ... seq_q seq_k") / (d_k**0.5)
     if mask is not None:
         attention_weights += torch.where(mask, 0, float("-inf"))
     softmax = torch.softmax(attention_weights, dim=-1)
     return softmax @ V
 
 
-class MultiHeadAttention(nn.Module):
-    def __init__(self, d_model, num_heads, device: torch.device | None = None, dtype: torch.dtype | None = None):
-        super().__init__()
-        assert d_model % num_heads == 0
-        mean, std = 0, 0.02  ## why make these choices
-        self.num_heads = num_heads
-        d_Q = d_K = d_V = d_model // num_heads
-        ## convention from the spec, note WO dimensions are flipped (out, in)
-        self.WQ = nn.Parameter(torch.empty(d_Q * num_heads, d_model, device=device, dtype=dtype))
-        self.WK = nn.Parameter(torch.empty(d_K * num_heads, d_model, device=device, dtype=dtype))
-        self.WV = nn.Parameter(torch.empty(d_V * num_heads, d_model, device=device, dtype=dtype))
-        self.WO = nn.Parameter(torch.empty(d_model, d_V * num_heads, device=device, dtype=dtype))
-        for p in (self.WQ, self.WK, self.WV, self.WO):
-            nn.init.normal_(p, mean=mean, std=std)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        seq_len = x.shape[-2]
-        ## need to pass the diagonal mask to pass the test.
-        diagonal_mask = torch.tril(torch.ones((seq_len, seq_len), dtype=torch.bool), diagonal=0)
-        Q = einsum(x, self.WQ, "batch ... sequence model, query model -> batch ... sequence query")
-        K = einsum(x, self.WK, "batch ... sequence model, key model -> batch ... sequence key")
-        V = einsum(x, self.WV, "batch ... sequence model, value model -> batch ... sequence value")
-        head_rep = lambda M: rearrange(M, "... seq_len (h d_head) -> ... h seq_len d_head", h=self.num_heads)
-        hQ, hK, hV = head_rep(Q), head_rep(K), head_rep(V)
-        mult_head_attention = rearrange(SPDA(hQ, hK, hV, diagonal_mask), "... h seq_len d_val -> ... seq_len (h d_val)")
-        return einsum(mult_head_attention, self.WO, "... hdval, ... model hdval -> ... model")
-
-
 class RoPE(nn.Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
         super().__init__()
-        i = torch.arange(0, max_seq_len)
-        k = torch.arange(1, d_k // 2 + 1)
+        i = torch.arange(0, max_seq_len, device=device)
+        k = torch.arange(1, d_k // 2 + 1, device=device)
         freq = 1 / theta ** ((2.0 * k - 2.0) / d_k)
         theta_ik = i[:, None] * freq[None, :]
         self.register_buffer("cos", theta_ik.cos(), persistent=False)
@@ -142,3 +121,48 @@ class RoPE(nn.Module):
         odd_out = sin * evens + cos * odds
         x_rope = rearrange(torch.stack((even_out, odd_out), dim=-1), "... blocks pair -> ... (blocks pair)")
         return x_rope
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(
+        self,
+        d_model,
+        num_heads,
+        max_seq_len,
+        rope: RoPE | None = None,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        super().__init__()
+        self.rope = rope  # positional embedding submodule
+
+        ## need to pass the diagonal mask to pass the test - create
+        diagonal_mask = torch.tril(torch.ones((max_seq_len, max_seq_len), device=device, dtype=torch.bool), diagonal=0)
+        self.register_buffer("mask", diagonal_mask, persistent=False)
+
+        assert d_model % num_heads == 0
+        mean, std = 0, 0.02  ## why make these choices
+        self.num_heads = num_heads
+        d_Q = d_K = d_V = d_model // num_heads
+        ## convention from the spec, note WO dimensions are flipped (out, in)
+        self.WQ = nn.Parameter(torch.empty(d_Q * num_heads, d_model, device=device, dtype=dtype))
+        self.WK = nn.Parameter(torch.empty(d_K * num_heads, d_model, device=device, dtype=dtype))
+        self.WV = nn.Parameter(torch.empty(d_V * num_heads, d_model, device=device, dtype=dtype))
+        self.WO = nn.Parameter(torch.empty(d_model, d_V * num_heads, device=device, dtype=dtype))
+        for p in (self.WQ, self.WK, self.WV, self.WO):
+            nn.init.normal_(p, mean=mean, std=std)
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor | None = None) -> torch.Tensor:
+        seq_len = x.shape[-2]
+        mask = self.mask[:seq_len, :seq_len]
+        Q = einsum(x, self.WQ, "batch ... seq d_model, d_qkv d_model -> batch ... seq d_qkv")
+        K = einsum(x, self.WK, "batch ... seq d_model, d_qkv d_model -> batch ... seq d_qkv")
+        V = einsum(x, self.WV, "batch ... seq d_model, d_qkv d_model -> batch ... seq d_qkv")
+        head_rep = lambda M: rearrange(M, "... seq (n_heads d_head) -> ... n_heads seq d_head", n_heads=self.num_heads)
+        hQ, hK, hV = head_rep(Q), head_rep(K), head_rep(V)
+        if self.rope is not None:
+            assert token_positions is not None
+            hK = self.rope(hK, token_positions)
+            hQ = self.rope(hQ, token_positions)
+        mult_head_attention = rearrange(SPDA(hQ, hK, hV, mask), "... n_heads seq d_head -> ... seq (n_heads d_head)")
+        return einsum(mult_head_attention, self.WO, "... d_v, ... d_model d_v -> ... d_model")
