14888659bd5b470752386d2fdb0557094b3c269c
ocanphys <oguzhan.can@outlook.com>
2026-07-23 13:13:59 -0700

    config to pin local volume

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+# Since config.py is at the root, its parent IS the root directory
+PROJECT_ROOT = Path(__file__).resolve().parent
+LOCAL_VOLUME = PROJECT_ROOT / 'volume'
